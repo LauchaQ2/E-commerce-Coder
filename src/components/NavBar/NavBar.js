8ec117b4387057d1dd20108bf84d09861b0807fd
Lautaro Quevedo <89219479+LauchaQ2@users.noreply.github.com>
2021-11-21 18:34:03 -0300

    Add files via upload

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../NavBar/NavBar.css';
-import logo from '../../assets/logomangas.png'
+import logo from '../../assets/logo en negro/logo_large.png'
 
 export default function NavBar(){
 return(
@@ -9,10 +9,10 @@ return(
             <img id="logo" src={logo} />
         </div>
         <ul className="menu">
-            <li><a className="linkStyle" href="index.html">Home</a></li>
-            <li><a className="linkStyle" href="products.html">Products</a></li>
-            <li><a className="linkStyle" href="contact.html">Contact</a></li>
-            <li><a className="linkStyle" href="about.html">About</a></li>
+            <li><a className="linkStyle" href="index.html">Inicio</a></li>
+            <li><a className="linkStyle" href="products.html">Productos</a></li>
+            <li><a className="linkStyle" href="contact.html">Contacto</a></li>
+            <li><a className="linkStyle" href="about.html">Nosotros</a></li>
         </ul>
         <ul className="menu2">
             <li><a href="login.html"> <img className="imgheader" src="https://i.ibb.co/xfHJPsc/carrito-de-compras.png" /></a></li>
